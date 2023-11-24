da2ecd2fab4a919992c2a324d8105d732104ff16
Moiz Khan <er.moiz2102@gmail.com>
2023-11-25 01:11:58 +0530

    code final

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
 public class Main {
+    public static class Product{
+         public int product(int x,int y){
+             return x*y;
+         }
+         public int product (int x,int y,int z){
+             return x*y*z;
+         }
+         public double product (double a,double b){
+             return a*b;
+         }
+    }
+public static void main (String[]args){
+    Product p=new Product();
+    System.out.println(p.product(2,3));
+    System.out.println(p.product(2,3,5));
+    System.out.println(p.product(2.0,3.0));
 
+}
 }
